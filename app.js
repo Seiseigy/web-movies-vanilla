df47d65b2ab470b4bcb70632b16e5698b7b48eb9
Sei Sebastian <18251746+Seiseigy@users.noreply.github.com>
2020-11-25 03:00:44 -0300

    Mostrar cartas de peliculas en pantalla

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 const form = document.querySelector('form');
 const input = document.querySelector('#searchTerm');
+const resultsSection = document.querySelector('#results');
 
 const API = 'https://www.thesuper.cloud/api/movies?movie=';
 
 form.addEventListener('submit', submittedForm);
 
-function getResults(searchTerm) {
+async function getResults(searchTerm) {
     const url = `${API}${searchTerm}`;
-    const results = [];
-    fetch(url)
-        .then(response => response.json())
-        .then(data => showResults(data.Search));
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.Search;
 };
 
-function submittedForm(event) {
+async function submittedForm(event) {
     event.preventDefault();
     const searchTerm = input.value;
-    getResults(searchTerm)
+    const results = await getResults(searchTerm)
+    showResults(results);
 };
 
 function showResults(results) {
-    console.log(results);
+    resultsSection.innerHTML = results.reduce((html, movie) => {
+        return html + `
+        <div class="card col-4 border-info mb-3" style="max-width: 20rem;">
+            <img class ="card-img-top" src="${movie.Poster} alt="${movie.Title}">
+            <div class="card-body">
+              <h4 class="card-title text-center">${movie.Title}</h4>
+              <p class="card-text text-center">${movie.Year}</p>
+            </div>
+        </div>
+        `;    
+    } ,'');
 };
 
 
