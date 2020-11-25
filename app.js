80852f97999f134f3185cf35d3f0577cc3bc2c27
Sei Sebastian <18251746+Seiseigy@users.noreply.github.com>
2020-11-25 01:53:12 -0300

    Consulta a API de peliculas funcionando

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-// console.log("Hola");
+const form = document.querySelector('form');
+const input = document.querySelector('#searchTerm');
+
+const API = 'https://www.thesuper.cloud/api/movies?movie=';
+
+form.addEventListener('submit', submittedForm);
+
+function getResults(searchTerm) {
+    const url = `${API}${searchTerm}`;
+    const results = [];
+    fetch(url)
+        .then(response => response.json())
+        .then(data => showResults(data.Search));
+};
+
+function submittedForm(event) {
+    event.preventDefault();
+    const searchTerm = input.value;
+    getResults(searchTerm)
+};
+
+function showResults(results) {
+    console.log(results);
+};
+
+
+
+
